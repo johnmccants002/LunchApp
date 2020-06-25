fd0f574244fe7f9ee13782cdf33db9b153091bbd
johnmccants002 <johnmccants002@gmail.com>
2020-06-24 19:55:34 -0700

    Update added Xcode Project

---
+++ LunchAppProjects/LunchAppProjects/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LunchAppProjects
+//
+//  Created by John McCants on 6/24/20.
+//  Copyright © 2020 John McCants. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
