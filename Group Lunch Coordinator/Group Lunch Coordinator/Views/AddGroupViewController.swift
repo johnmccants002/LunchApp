2479a293505e346531fef4772ec736b3c175899c
johnmccants002 <johnmccants002@gmail.com>
2020-06-25 21:30:24 -0700

    updated Johns stuff

--- Group Lunch Coordinator/Group Lunch Coordinator/Views/AddGroupViewController.swift
+++ Group Lunch Coordinator/Group Lunch Coordinator/Views/AddGroupViewController.swift
@@ -9,21 +9,27 @@
 import UIKit
 
 class AddGroupViewController: UIViewController {
-
     
+    var groupController : GroupController?
+
   
     @IBOutlet weak var groupTextField: UITextField!
     
     @IBOutlet weak var addGroupButton: UIButton!
     
     @IBAction func addGroupButtonTapped(_ sender: Any) {
+        createGroup()
+        dismiss(animated: true) {
+            guard let groupController = self.groupController else {return}
+            groupController.loadFromPersistenceStore()
+        }
+        
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
@@ -36,7 +42,34 @@ class AddGroupViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func createGroup() {
+        guard let groupText = groupTextField.text, groupText.isEmpty == false, let group = groupController else { return }
+        let newGroup = Group(name: groupText, totalMembers: 1, joined: true)
+        group.groups.append(newGroup)
+        group.saveToPersistenceStore()
+        
+    }
 
 }
+
+
+
+
+//extension AddGroupViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+//    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+//        1
+//    }
+//
+//    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+////        timeGroup.count
+//    }
+//
+//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+////        timeGroup[row]
+//    }
+//
+//
+//}
 
 
